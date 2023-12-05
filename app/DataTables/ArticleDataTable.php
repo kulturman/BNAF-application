8991ade5fe1a1a5259bde99c551abbfe02db4d6a
kulturman <arnaudbakyono@gmail.com>
2023-12-05 06:28:34 +0000

    Try hacky solution on Article DT

--- app/DataTables/ArticleDataTable.php
+++ app/DataTables/ArticleDataTable.php
@@ -51,7 +51,7 @@ class ArticleDataTable extends DataTable
     {
         return $this->builder()
             ->columns($this->getColumns())
-            ->minifiedAjax(customUrl(request()->path())
+            ->minifiedAjax(customUrl(request()->path()))
             ->addAction(['width' => '120px', 'printable' => false, 'title' => 'Actions'])
             ->parameters([
                 'dom' => 'Bfrtip',
