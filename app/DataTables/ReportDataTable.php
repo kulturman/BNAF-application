9476e1312a5aca76b74cb331a2574e66da29dd57
kulturman <arnaudbakyono@gmail.com>
2023-12-05 06:31:26 +0000

    Use hacky solution as workaround

--- app/DataTables/ReportDataTable.php
+++ app/DataTables/ReportDataTable.php
@@ -55,7 +55,7 @@ class ReportDataTable extends DataTable
     {
         return $this->builder()
             ->columns($this->getColumns())
-            ->minifiedAjax()
+            ->minifiedAjax(customUrl(request()->path()))
             ->addAction(['width' => '120px', 'printable' => false, 'title' => 'Actions'])
             ->parameters([
                 'dom' => 'Bfrtip',
